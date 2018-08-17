4f572b1a7dfe5a6c1de495cc5eead52ee336a2f7
Taylor Sam <taylorsam209@yahoo.com>
2018-08-17 11:59:49 -0700

    Created a header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
-import {MatInputModule, MatCardModule} from '@angular/material';
+import {MatInputModule, MatCardModule, MatButtonModule, MatToolbarModule} from '@angular/material';
 
 
 import { AppComponent } from './app.component';
 import {PostCreateComponent} from './posts/post-create/post-create.component';
+import {HeaderComponent} from './header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostCreateComponent
+    PostCreateComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
     MatInputModule,
-    MatCardModule
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
