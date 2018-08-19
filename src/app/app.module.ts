c417a77e64a7793ed8b8081a5ee83385534dcbef
Taylor Sam <taylorsam209@yahoo.com>
2018-08-19 13:39:01 -0700

    Added list logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import {MatInputModule, MatCardModule, MatButtonModule, MatToolbarModule} from '@angular/material';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import {
+  MatInputModule,
+  MatCardModule,
+  MatButtonModule,
+  MatToolbarModule,
+  MatExpansionModule
+} from "@angular/material";
 
-
-import { AppComponent } from './app.component';
-import {PostCreateComponent} from './components/posts/post-create/post-create.component';
-import {PostListComponent} from './components/posts/post-list/post-list.component';
-import {HeaderComponent} from './components/header/header.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from "./app.component";
+import { PostCreateComponent } from "./components/posts/post-create/post-create.component";
+import { PostListComponent } from "./components/posts/post-list/post-list.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
     AppComponent,
     PostCreateComponent,
     PostListComponent,
-    HeaderComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
@@ -24,9 +29,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatInputModule,
     MatCardModule,
     MatButtonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/posts/post-list/post-list.component.ts
+++ src/app/components/posts/post-list/post-list.component.ts
@@ -2,9 +2,18 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-post-list',
-  templateUrl: './post-list.component.html';
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
 })
 
 export class PostListComponent {
+  // posts = [
+  //   {title: 'First Post', content: 'This first post\s content'},
+  //   {title: 'Second Post', content: 'This Second post\s content'},
+  //   {title: 'Third Post', content: 'This Third post\s content'},
+  // ];
+
+  posts = [];
+
 
 }
